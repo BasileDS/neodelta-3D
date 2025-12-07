ae7dda777522e71d86c2396527bc913dd8305f46
BasileDS <basile.d.santos@gmail.com>
2025-12-07 22:50:45 +0100

    feat: make clipPathId optional and refactor VectorLetter component structure

--- src/components/vector/VectorLetter.tsx
+++ src/components/vector/VectorLetter.tsx
@@ -28,7 +28,7 @@ interface VectorLetterProps {
   
   // Filter and gradient IDs (unique per letter)
   filterId: string;
-  clipPathId: string;
+  clipPathId?: string;
   gradientId: string;
   
   // Foreignobject dimensions for backdrop blur
@@ -59,44 +59,69 @@ export default function VectorLetter({
   glowDx2 = 2.32229,
   glowDy2 = 3.16675,
   filterId,
-  clipPathId,
   gradientId,
   foreignX,
   foreignY,
   foreignWidth,
   foreignHeight
 }: VectorLetterProps) {
+  // Convert SVG path to CSS clip-path polygon if possible
+  // For now, we'll use the backdrop on the wrapper with inline SVG for clipping
+  const svgDataUrl = `data:image/svg+xml,${encodeURIComponent(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="${viewBox}"><path d="${clipPath}"/></svg>`)}`;
+  
   return (
-    <div data-svg-wrapper data-layer="Vector" className="Vector">
-      <svg width={width} height={height} viewBox={viewBox} fill="none" xmlns="http://www.w3.org/2000/svg">
-        <foreignObject x={foreignX} y={foreignY} width={foreignWidth} height={foreignHeight}>
-          <div style={{ backdropFilter: `blur(${backdropBlur})`, clipPath: `url(#${clipPathId})`, height: '100%', width: '100%' }}></div>
-        </foreignObject>
-        
-        <g filter={`url(#${filterId})`} data-figma-bg-blur-radius="15.16">
-          <path 
-            d={fillPath} 
-            fill={fillColor} 
-            fillOpacity={fillOpacity} 
-            style={{ mixBlendMode: blendMode }}
-          />
-          <path 
-            d={strokePath} 
-            stroke={`url(#${gradientId})`} 
-            strokeWidth={strokeWidth} 
-            strokeOpacity={strokeOpacity} 
-            style={{ mixBlendMode: blendMode }}
-          />
-        </g>
-        
+    <div
+      data-svg-wrapper
+      data-layer="Vector"
+      className="Vector"
+      style={{
+        position: 'relative',
+        width: `${width}px`,
+        height: `${height}px`,
+      }}
+    >
+      {/* Backdrop blur layer */}
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          backdropFilter: `blur(${backdropBlur})`,
+          WebkitBackdropFilter: `blur(${backdropBlur})`,
+          WebkitMaskImage: `url("${svgDataUrl}")`,
+          maskImage: `url("${svgDataUrl}")`,
+          WebkitMaskSize: 'contain',
+          maskSize: 'contain',
+          WebkitMaskRepeat: 'no-repeat',
+          maskRepeat: 'no-repeat',
+          WebkitMaskPosition: 'center',
+          maskPosition: 'center',
+        }}
+      />
+      
+      {/* SVG letter on top */}
+      <svg
+        width={width}
+        height={height}
+        viewBox={viewBox}
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+        }}
+      >
         <defs>
-          <filter 
-            id={filterId} 
-            x={foreignX} 
-            y={foreignY} 
-            width={foreignWidth} 
-            height={foreignHeight} 
-            filterUnits="userSpaceOnUse" 
+          <filter
+            id={filterId}
+            x={foreignX}
+            y={foreignY}
+            width={foreignWidth}
+            height={foreignHeight}
+            filterUnits="userSpaceOnUse"
             colorInterpolationFilters="sRGB"
           >
             <feFlood floodOpacity="0" result="BackgroundImageFix"/>
@@ -105,8 +130,8 @@ export default function VectorLetter({
             <feOffset dx={glowDx1 * glowIntensity} dy={glowDy1 * glowIntensity}/>
             <feGaussianBlur stdDeviation={2.31 * glowIntensity}/>
             <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
-            <feColorMatrix 
-              type="matrix" 
+            <feColorMatrix
+              type="matrix"
               values={`0 0 0 0 ${glowColor === 'white' ? '1' : '0'} 0 0 0 0 ${glowColor === 'white' ? '1' : '0'} 0 0 0 0 ${glowColor === 'white' ? '1' : '0'} 0 0 0 ${glowOpacity} 0`}
             />
             <feBlend mode="lighten" in2="shape" result="effect1_innerShadow"/>
@@ -114,16 +139,12 @@ export default function VectorLetter({
             <feOffset dx={glowDx2 * glowIntensity} dy={glowDy2 * glowIntensity}/>
             <feGaussianBlur stdDeviation={4.62 * glowIntensity}/>
             <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
-            <feColorMatrix 
-              type="matrix" 
+            <feColorMatrix
+              type="matrix"
               values={`0 0 0 0 ${glowColor === 'white' ? '1' : '0'} 0 0 0 0 ${glowColor === 'white' ? '1' : '0'} 0 0 0 0 ${glowColor === 'white' ? '1' : '0'} 0 0 0 ${glowOpacity} 0`}
             />
             <feBlend mode="lighten" in2="effect1_innerShadow" result="effect2_innerShadow"/>
           </filter>
-          
-          <clipPath id={clipPathId} transform={`translate(${-foreignX} ${-foreignY})`}>
-            <path d={clipPath}/>
-          </clipPath>
           
           <linearGradient id={gradientId} x1="-67.5093" y1="0" x2="67.5093" y2="0" gradientUnits="userSpaceOnUse">
             <stop stopColor="white"/>
@@ -131,6 +152,22 @@ export default function VectorLetter({
             <stop offset="1" stopColor="white" stopOpacity="0.2"/>
           </linearGradient>
         </defs>
+        
+        <g filter={`url(#${filterId})`} data-figma-bg-blur-radius="15.16">
+          <path
+            d={fillPath}
+            fill={fillColor}
+            fillOpacity={fillOpacity}
+            style={{ mixBlendMode: blendMode }}
+          />
+          <path
+            d={strokePath}
+            stroke={`url(#${gradientId})`}
+            strokeWidth={strokeWidth}
+            strokeOpacity={strokeOpacity}
+            style={{ mixBlendMode: blendMode }}
+          />
+        </g>
       </svg>
     </div>
   );
